8615eea8a8453bf7ed481be1fcebe3258aa8d0ab
MChenryDev <henry4621.com@gmail.com>
2023-10-06 22:11:20 -0600

    tercer commit

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,11 +1,22 @@
 <?php
-$host = 'localhost'; // Cambia esto si tu base de datos está en un servidor remoto
-$dbname = 'banca_linea';
-$username = 'root';
-$password = '';
+session_start();
+// $host = 'localhost'; 
+// $dbname = 'banca_linea';
+// $username = 'root';
+// $password = '';
 
-// Crear conexión
-$conn = new mysqli($host, $username, $password, $dbname);
+$DB_HOST=$_ENV["DB_HOST"];
+$DB_USER=$_ENV["DB_USER"];
+$DB_PASSWORD=$_ENV["DB_PASSWORD"];
+$DB_NAME=$_ENV["DB_NAME"];
+$DB_PORT=$_ENV["DB_PORT"];
+
+
+// CONEXION LOCAL
+//$conn = new mysqli($host, $username, $password, $dbname);
+
+// CONEXION EN NUBE
+$conn = new mysqli("DB_HOST", "DB_USER", "DB_PASSWORD", "DB_NAME", "DB_PORT");
 
 // Verificar la conexión
 if ($conn->connect_error) {
