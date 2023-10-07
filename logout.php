ead1c289fd243911a03df3713684220dab0a06a0
MChenryDev <henry4621.com@gmail.com>
2023-10-07 00:31:38 -0600

    6to commit

--- logout.php
+++ logout.php
@@ -8,7 +8,6 @@ session_unset();
 unset($_SESSION["nameUser"]);
 //$_SESSION = [];
 
-echo "La sesión ha sido destruida.";
 
 header("Location: index.php");  // Redirigir a la página de inicio de sesión u otra página apropiada
 
